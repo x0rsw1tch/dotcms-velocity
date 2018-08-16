b3d2dfe6248299ee5e31b7949e7a34920c39a46b
Tom Kisha <x0rsw1tch@gmail.com>
2018-08-16 13:06:04 -0400

    context.xml

--- install-dotcms.sh
+++ install-dotcms.sh
@@ -27,11 +27,17 @@ POSTGRESQL_CONFIGURED=false
 ## STEP 1: Prerequisites
 ##
 
+if [ "$(whoami)" != "root" ]; then
+	echo "Must be root"
+	exit 1
+fi
+
 
 read -p "Is the OS up to date?" -n 1 -r OS_UP_TO_DATE
 echo    # (optional) move to a new line
 if [[ $OS_UP_TO_DATE =~ ^[Yy]$ ]] ; then
-    echo 'Which version of PostgreSQL do you want to use?'
+	echo
+	echo 'Which version of PostgreSQL do you want to use?'
     echo '1. Version 9 (default/recommended)'
     echo '2. Version 10 (experimental)'
     echo
@@ -59,6 +65,9 @@ if [[ $OS_UP_TO_DATE =~ ^[Yy]$ ]] ; then
             fi
         fi
     fi
+	echo
+    read -p "Are we going to use SSL (y/n)?" -n 1 -r DOTCMS_USE_SSL
+    echo
 else
     read -p "Do you want to update the OS?" -n 1 -r UPDATE_OS_CHOICE
     if [[ $UPDATE_OS_CHOICE =~ ^[Yy]$ ]] ; then
@@ -87,21 +96,22 @@ else
 fi
 
 
-##
-## STEP 2: Get Info from User
-##
-
 if [ $PREREQUISITE_PACKAGES_INSTALLED = true ] ; then
+
+	DOTCMS_DATABASE_NAME_DEFAULT="dotcms"
+	DOTCMS_DATABASE_USER_DEFUALT="dotcms"
 
 	echo
 	echo '##################################'
 	echo '## STEP 2: Users and passwords  ##'
 	echo '##################################'
 	echo
-	echo "Both the Linux user and Database user for dotCMS will be \"dotcms\""
-	echo
-	read -p "Enter dotCMS Linux User's Password:" -r DOTCMS_LINUX_USER_PASSWORD
-	read -p "Enter dotCMS Database User's Password:" -r DOTCMS_DATABASE_PASSWORD
+	read -p "Enter dotCMS Linux User's Password: " -r DOTCMS_LINUX_USER_PASSWORD
+	read -p "Enter dotCMS Database Name [dotcms]: " -r DOTCMS_DATABASE_NAME
+	DOTCMS_DATABASE_NAME="${DOTCMS_DATABASE_NAME:-$DOTCMS_DATABASE_NAME_DEFAULT}"
+	read -p "Enter dotCMS Database User [dotcms]: " -r DOTCMS_DATABASE_USER
+	DOTCMS_DATABASE_USER="${DOTCMS_DATABASE_USER:-$DOTCMS_DATABASE_USER_DEFUALT}"
+	read -p "Enter dotCMS Database User's Password: " -r DOTCMS_DATABASE_PASSWORD
 
 	groupadd dotcms
 	useradd -M dotcms -g dotcms
@@ -113,11 +123,6 @@ if [ $PREREQUISITE_PACKAGES_INSTALLED = true ] ; then
 	fi
 
 fi
-
-
-##
-## STEP 3: Configure Database
-##
 
 if [ $DOTCMS_USER_CONFIGURED = true ] ; then
 
@@ -243,7 +248,7 @@ fi
 ## STEP 4: Setup dotCMS
 ##
 
-if [ $DOTCMS_USER_CONFIGURED = true ] ; then
+if [[ $DOTCMS_USER_CONFIGURED = true ]] ; then
 
 	echo
 	echo '##########################'
@@ -251,7 +256,165 @@ if [ $DOTCMS_USER_CONFIGURED = true ] ; then
 	echo '##########################'
 	echo
 
+    echo 'Which version of dotCMS do you want to install ie: 3.7.2, 4.3.3, 5.0.0?'
+	echo 'dotCMS 2.x versions not supported'
+	echo
+    read -p "Version: " -r DOTCMS_VERSION_CHOICE
+	
+	if [[ $DOTCMS_VERSION_CHOICE == 5.0.0 ]] || [[ $DOTCMS_VERSION_CHOICE == 4.3.3 ]] ; then
+	echo
+	echo 'Choose Starter Package:'
+	echo
+	echo '1. Vanilla                   All Versions'
+	echo '2. Minimal with Utilities    4.3.3 only'
+	echo '3. Minimal                   4.3.3/5.0.0'
+	echo
+	read -p "Starter Choice: " -r DOTCMS_STARTER_CHOICE
 
+	if [[ $DOTCMS_VERSION_CHOICE == 5.* ]] ; then
+		DOTCMS_TOMCAT_VERSION=8.5.32
+	else
+		DOTCMS_TOMCAT_VERSION=8.0.18
+	fi
 
+	DOTCMS_STARTER_CHOICE_VALID=false
+	
+	if [[ $DOTCMS_STARTER_CHOICE = 1 ]]
+		DOTCMS_STARTER_CHOICE_VALID=true
+	fi
+
+	if [[ $DOTCMS_STARTER_CHOICE = 2 ]] && [[ $DOTCMS_VERSION_CHOICE = 4.3.3 ]]
+		DOTCMS_STARTER_CHOICE_VALID=true
+	fi
+
+	if [[ $DOTCMS_STARTER_CHOICE = 3 ]] && [[ $DOTCMS_VERSION_CHOICE = 5.0.0 ]]
+		DOTCMS_STARTER_CHOICE_VALID=true
+	fi
+
+	if [[ $DOTCMS_STARTER_CHOICE_VALID = false ]]
+		echo
+		echo 'Invalid dotCMS version & Starter package selected, switching to vanilla'
+		echo 
+		read -p "Is this okay?" -n 1 -r DOTCMS_SWITCH_TO_VANILLA
+		if [[ $DOTCMS_SWITCH_TO_VANILLA =~ ^[Yy]$ ]] ; then
+			DOTCMS_STARTER_CHOICE=1
+		else
+			echo "Unable to continue."
+			exit 1
+		fi
+	fi
+
+	if [[ $DOTCMS_STARTER_CHOICE_VALID = true ]]
+
+		echo 
+		echo "Making PID Directory"
+		echo
+		mkdir -p /var/run/dotcms
+		chown dotcms:dotcms /var/run/dotcms
+
+		echo 
+		echo "Making /opt/dotcms"
+		echo
+		mkdir -p /opt/dotcms
+		cd /opt/dotcms
+
+		
+		DOTCMS_VALID_DOWNLOAD=false
+		echo 
+		echo "Downloading dotCMS"
+		echo
+		if wget http://dotcms.com/physical_downloads/release_builds/dotcms_${DOTCMS_VERSION_CHOICE}.tar.gz; then
+			DOTCMS_VALID_DOWNLOAD=true
+		else
+			echo "Unable to download dotCMS, aborting..."
+			exit 1
+		fi
+
+		if [[ $DOTCMS_VALID_DOWNLOAD = true ]] ; then
+
+			echo 
+			echo "Unpacking dotCMS"
+			echo
+			if tar -zxvf dotcms_${DOTCMS_VERSION_CHOICE}.tar.gz; then
+				DOTCMS_EXTRACTED=true
+			fi
+
+			if [[ $DOTCMS_VERSION_CHOICE = 5.0.0 ]] ; then
+				if [[ $DOTCMS_STARTER_CHOICE = 3 ]] ; then
+					DOTCMS_STARTER_FILE="dotcms-5.0.0_minimal.zip"
+					wget https://github.com/x0rsw1tch/dotcms-starters/raw/master/dotcms-5.0.0_minimal.zip
+				fi
+			fi
+
+			if [[ $DOTCMS_VERSION_CHOICE = 4.3.3 ]] ; then
+				if [[ $DOTCMS_STARTER_CHOICE = 3 ]] ; then
+					DOTCMS_STARTER_FILE="dotcms-4.3.3_minimal.zip"
+					wget https://github.com/x0rsw1tch/dotcms-starters/raw/master/dotcms-4.3.3_minimal.zip
+				fi
+				if [[ $DOTCMS_STARTER_CHOICE = 2 ]] ; then
+					DOTCMS_STARTER_FILE="dotcms-4.3.3_minimal.zip"
+					wget https://github.com/x0rsw1tch/dotcms-starters/raw/master/dotcms-4.3.3_with-tools_0.31.zip
+				fi
+			fi
+
+	fi
+fi
+
+if [[ $DOTCMS_EXTRACTED = true]] ; then
+
+	echo
+	echo '##############################'
+	echo '## STEP 5: Configure dotCMS ##'
+	echo '##############################'
+	echo
+
+	echo
+	echo 'Making plugin folders...'
+	echo
+	mkdir -p plugins/com.dotcms.config/ROOT/bin
+	mkdir -p plugins/com.dotcms.config/ROOT/dotserver/${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/META-INF
+	mkdir -p plugins/com.dotcms.config/ROOT/dotserver/${DOTCMS_TOMCAT_VERSION}/conf/
+	mkdir -p plugins/com.dotcms.config/ROOT/dotserver/${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/WEB-INF/classes
+
+	echo
+	echo 'Copying config files...'
+	echo
+	cp dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/META-INF/context.xml plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/META-INF
+	cp dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/conf/server.xml plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/conf
+	cp bin/startup.sh plugins/com.dotcms.config/ROOT/bin
+
+	
+	echo
+	echo 'Editing context.xml: Disabling H2 and adding database connection information...'
+	echo
+
+	sed -i '/<!-- H2-->/c \ \ \ \ <\!-- SECTION EDITED WITH DOTCMS INSTALLER -->\n\ \ \ \ <!-- H2'  plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/context.xml
+	sed -i '0,/abandonWhenPercentageFull="50"\/>/s/abandonWhenPercentageFull="50"\/>/abandonWhenPercentageFull="50"\/>\n\ \ \ \ -->/'  plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/context.xml
+	sed -i '/<!-- POSTGRESQL/c \ \ \ \ <\!-- SECTION EDITED WITH DOTCMS INSTALLER -->\n\ \ \ \ <!-- POSTGRESQL -->'  plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/context.xml
+	sed -i '/url=\"jdbc\:postgresql\:\/\/localhost\/dotcms\"/c \ \ \ \ \ \ \ \ \ \ url=\"jdbc\:postgresql\:\/\/localhost\/${DOTCMS_DATABASE_NAME}\"'  plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/context.xml
+	sed -i '/username="{your db user}" password="{your db password}"/c \ \ \ \ \ \ \ \ \ \ username="${DOTCMS_DATABASE_USER}" password="${DOTCMS_DATABASE_PASSWORD}" maxActive="60" maxIdle="10" maxWait="60000"'  plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/context.xml
+	sed -i '0,/^-->$/s/^-->$//'  plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/context.xml
+
+	echo
+	echo "Let's verify context.xml...."
+	echo
+	echo '************************************************************'
+	cat plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/context.xml
+	echo '************************************************************'
+	echo
+	read -p "Does this look correct (y/n)?" -n 1 -r CONTEXT_LOOKS_GOOD
+
+	if [[ $CONTEXT_LOOKS_GOOD =~ ^[Nn]$ ]] ; then
+		echo "Let's edit context.xml manually then..."
+		read -p "Press enter to continue"
+		nano plugins/com.dotcms.config/ROOT/dotserver/tomcat-${DOTCMS_TOMCAT_VERSION}/webapps/ROOT/context.xml
+	fi
+	
+	if [[ $DOTCMS_USE_SSL =~ ^[Yy]$ ]] ; then
+		echo
+		echo 'Editing server.xml: Adding SSL configs...'
+		echo
+	fi
 
 fi
+
